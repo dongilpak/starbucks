ead32c9751f08961e6cb2f1d717e094700d670e2
dongilpak <ehddlf2395@gmail.com>
2023-06-15 22:02:13 +0900

    메인화면 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
 import './App.css';
+import Vessel from './components/Vessel/vessel';
+import logo from './assets/logo.png';
 
 function App() {
-    return <div></div>;
+    return (
+        <div id='app'>
+            <div id='component-root'>
+                <div className='component-wrapper'>
+                    <img className='logo' src={logo} alt='logo' />
+                    <h1 className='component__title'>스타벅스</h1>
+                    <div className='component__intro'>
+                        <p>커피 이상의</p>
+                        <p>특별한 경험을 소개합니다.</p>
+                    </div>
+                </div>
+            </div>
+            <Vessel />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Vessel/vessel.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './vessel.css';
+
+const vessel = () => {
+    return <div id='application'></div>;
+};
+
+export default vessel;
